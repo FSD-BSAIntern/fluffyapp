4cfd95f858bae7a56b67d66df75129ef2df03a60
BSA Intern <BSAIntern@ad.feedingsandiego.org>
2026-03-09 14:38:45 -0700

    Ver 1.2.0 - KPI App Addition

--- Start.py
+++ Start.py
@@ -18,10 +18,11 @@ login_page = st.Page(login, title="Log in", icon=":material/login:")
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 ceres6_page = st.Page("pages/Ceres6App/1_Ceres6_Search.py",  title = "Ceres6 Query Tool", icon=":material/search:")
 order_trends_page = st.Page("pages/MemberOrderTrendsApp/2_Order_Trends.py", title = "Member Distribution Trends", icon=":material/trending_up:")
+kpi_showcase_page = st.Page("pages/KPIApp/3_KPI_Fulfillment_Showcase.py", title="KPI Fulfillment Showcase", icon=":material/insights:")
 hompage_page = st.Page("pages/Home.py", title="Home", icon=":material/house:")
 
 if st.session_state.logged_in:
-    pg = st.navigation([hompage_page, ceres6_page, order_trends_page, logout_page])
+    pg = st.navigation([hompage_page, ceres6_page, order_trends_page, kpi_showcase_page, logout_page])
     
 else:    
     pg = st.navigation([hompage_page, login_page])
