29d94bc9207995aa1448a36c5b72648caf28ac18
BSA Intern <BSAIntern@ad.feedingsandiego.org>
2026-03-09 11:02:26 -0700

    Fatal bug fix for page navigation

--- Start.py
+++ Start.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.write("Welcome to FluffyApp! Please select a page from the sidebar to get started.")
 # Page navigation
 pages = [
-    st.Page("pages/Ceres6App/1_Ceres6_Search.py", "Ceres6 Search"),
-    st.Page("pages/MemberOrderTrendsApp/2_app.py", "Member Order Trends")
+    st.Page("pages/Ceres6App/1_Ceres6_Search.py",  title =   "Ceres6 Search"),
+    st.Page("pages/MemberOrderTrendsApp/2_app.py", title =   "Member Order Trends")
 ]
 
 # Add pages to sidebar
