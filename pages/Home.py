46b3e4b4a98d9ae598d2434b8415d5f18a69a3bb
BSA Intern <BSAIntern@ad.feedingsandiego.org>
2026-03-09 12:48:17 -0700

    Stable release 1.1.0  - updates to Homepage & tool navigation

--- pages/Home.py
+++ pages/Home.py
@@ -7,8 +7,8 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Welcome to FluffyApp!")
-st.write("Please select a page from the sidebar to get started.")
+st.title("Welcome to FluffyApp! 🥑")
+st.write("Please select a page from the sidebar 👈 to get started.")
 
 st.sidebar.header("About FluffyApp")
 
@@ -17,11 +17,11 @@ FluffyApp is an all-in-one interface specifically for Feeding San Diego.
 
 It currently has two main features:
 
-**1. Ceres6 Query Tool**  
+**1. Ceres6 Query Tool** 🔍  
 A user-friendly interface for querying the Ceres6 database, allowing users to locate 
 specific information in reports about food distribution, member organizations, and more.
 
-**2. Member Distribution Trends**  
+**2. Member Distribution Trends** 📈 
 A tool for visualizing and analyzing trends in member or regional distribution over time.
 You also have the option to download a copy of the report for further analysis.
 """)
