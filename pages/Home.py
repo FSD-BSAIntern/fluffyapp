7aef4a7e2112f2624f3425bd09dea3d2a7644ff7
FSD-BSAIntern <bsaintern@feedingsandiego.org>
2026-03-19 11:57:27 -0700

    Updates to Home page
    Added a new feature for KPI Fulfillment Showcase to analyze key performance metrics and improve operations.

--- pages/Home.py
+++ pages/Home.py
@@ -18,10 +18,12 @@ Fluffy is an all-in-one interface specifically for Feeding San Diego.
 It currently has two main features:
 
 **1. Ceres6 Query Tool** 🔍 \\
-A user-friendly interface for querying the Ceres6 database, allowing users to locate 
-specific information in reports about food distribution, member organizations, and more.
+A user-friendly interface for querying the Ceres6 database, allowing users to locate specific information in reports about food distribution, member organizations, and more.
 
 **2. Member Distribution Trends** 📈 \\
-A tool for visualizing and analyzing trends in member or regional distribution over time.
-You also have the option to download a copy of the report for further analysis.
+A tool for visualizing and analyzing trends in member or regional distribution over time. You also have the option to download a copy of the report for record-keeping.
+
+**3. KPI Fulfillment Showcase** 🎯 \\
+A prototype showcase for an archived project with Fulfillment for analyzing key performance metrics, such as daily and weekly case or pallet movement over time. The goal is for new metrics to guide future operations to continously identify pain points and improve on them.
+
 """)
